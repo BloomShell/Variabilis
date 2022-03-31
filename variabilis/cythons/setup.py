5899d9f3e3c2e1ea5ff88c44a98697cbbedaf489
Rtas <77837516+PietroAminPuddu@users.noreply.github.com>
2022-03-31 17:08:39 +0200

    Delete variabilis/cythons directory

--- variabilis/cythons/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-import numpy
-from Cython.Build import cythonize
-
-setup(
-    ext_modules= cythonize("recursions.pyx",
-    compiler_directives={"boundscheck": False, "nonecheck": False, "cdivision": True}),
-    include_dirs=[numpy.get_include()]
-)
